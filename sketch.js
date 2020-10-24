84c8f378a7fef7bdb045b0db429f865a7ebd6060
AanchalAggarwal512 <73344464+AanchalAggarwal512@users.noreply.github.com>
2020-10-24 17:40:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,103 @@
+var ground, invisibleGround;
+var monkey , monkey_running;
+var banana ,bananaImage, obstacle, obstacleImage;
+var bananaGroup, obstacleGroup;
+var score, survivalTime;
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  
+  createCanvas (400,400);
+  
+  monkey = createSprite(80,315,20,20);
+  monkey.addAnimation("running",monkey_running);
+  monkey.scale = 0.1;
+  
+  ground = createSprite(400,350,900,10);
+  ground.velocityX = -4;
+    
+  invisibleGround = createSprite(200,350,400,10);
+  invisibleGround.visible = false;
+  
+  bananaGroup = new Group();
+  obstacleGroup = new Group();
+  
+  score = 0;
+  survivalTime = 0; 
+}
+
+
+function draw() {
+
+  background("lightblue");
+  
+   if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+  
+  if(keyDown("space") ) { 
+    monkey.velocityY = -12; 
+  } 
+  monkey.velocityY = monkey.velocityY + 0.8;
+  
+  
+  stroke("white");
+  textSize(20);
+  fill("white");
+  text("Score: "+ score, 300, 50);
+  
+  stroke("black");
+  textSize(20);
+  fill("black");
+  survivalTime = Math.ceil(frameCount/frameRate())
+  text("Survival Time: "+ survivalTime, 50,50);
+  
+  food();
+  obstacles();
+  
+  monkey.collide(invisibleGround);
+  
+  drawSprites();
+  
+}
+
+function food(){
+  if (frameCount % 80 === 0){
+     banana = createSprite(200,160,20,20);
+     banana.y = Math.round(random(120,200));
+     banana.addImage(bananaImage);
+     banana.scale = 0.1;
+     banana.velocityX = -3;
+     banana.lifetime = 200;
+    bananaGroup.add(banana);
+  } 
+  
+  
+}
+
+function obstacles(){
+  if (frameCount % 300 === 0){
+    obstacle = createSprite(400,165,10,40);
+    obstacle.addImage(obstacleImage);
+    obstacle.scale = 0.11;
+    obstacle.velocityX = -4;
+    obstacle.lifetime = 300;
+  obstacleGroup.add(obstacle);
+     }
+}
+
+
+
+
+ 
